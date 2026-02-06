1375d3a95a2e0a61f4b0f83226e275b2a1e2c2d3
gfloriano11 <gflorianodev@gmail.com>
2026-02-06 20:36:27 -0300

    feat: send raw token to decoder method

--- src/main/java/org/mokirim/infrastructure/security/authentication/AuthenticationFilter.java
+++ src/main/java/org/mokirim/infrastructure/security/authentication/AuthenticationFilter.java
@@ -9,6 +9,7 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
@@ -21,14 +22,15 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) {
-		String authHeader = requestContext.getHeaderString("authorization");
+		String authHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 
 		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
 			requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
 			return;
 		}
 
-		final var token = tokenDecoder.decode(authHeader);
+		final var rawToken = authHeader.substring("Bearer ".length());
+		final var token = tokenDecoder.decode(rawToken);
 		final var mokirimPrincipal = new MokirimPrincipal(token.getUserId(), token.getUsername());
 		final var securityContext = new MokirimSecurityContext(mokirimPrincipal);
 
