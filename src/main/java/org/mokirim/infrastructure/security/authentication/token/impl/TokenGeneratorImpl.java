0bb0e86d321140c9b44d6fc5ae36e43fef044df1
gfloriano11 <gflorianodev@gmail.com>
2026-02-10 23:09:05 -0300

    feat: improve token generator

--- src/main/java/org/mokirim/infrastructure/security/authentication/token/impl/TokenGeneratorImpl.java
+++ src/main/java/org/mokirim/infrastructure/security/authentication/token/impl/TokenGeneratorImpl.java
@@ -1,5 +1,6 @@
 package org.mokirim.infrastructure.security.authentication.token.impl;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -10,22 +11,31 @@ import org.mokirim.infrastructure.security.authentication.token.TokenGenerator;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class TokenGeneratorImpl implements TokenGenerator {
 
 	@ConfigProperty(name = "token.secret")
-	Key SECRET;
+	String SECRET;
 
 	@Override
 	public String handle() {
+		final Key secretToKey = getSecretKey();
 		Instant now = Instant.now();
 		String token = Jwts.builder()
-			.setSubject("550e8400-e29b-41d4-a716-446655440000") // TODO: use userId
+			.setSubject("550e8400-e29b-41d4-a716-446655440000")
 			.setIssuedAt(Date.from(now))
-			.setExpiration(Date.from(now.plus(1L, ChronoUnit.WEEKS)))
-			.signWith(SECRET, SignatureAlgorithm.HS256)
+			.setExpiration(Date.from(now.plus(7, ChronoUnit.DAYS)))
+			.signWith(secretToKey, SignatureAlgorithm.HS256)
 			.compact();
 
+		// TODO: use userId
 		return token;
 	}
+
+	private Key getSecretKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
 }
