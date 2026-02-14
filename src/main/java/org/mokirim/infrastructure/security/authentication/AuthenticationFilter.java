d9226bd4c869b0fd81fe553e1ec56296985dfbfe
gfloriano11 <gflorianodev@gmail.com>
2026-02-13 23:54:47 -0300

    feat: improve authentication filter to decode jwt token

--- src/main/java/org/mokirim/infrastructure/security/authentication/AuthenticationFilter.java
+++ src/main/java/org/mokirim/infrastructure/security/authentication/AuthenticationFilter.java
@@ -1,14 +1,18 @@
 package org.mokirim.infrastructure.security.authentication;
 
 import org.mokirim.infrastructure.security.authentication.token.TokenDecoder;
+import org.mokirim.infrastructure.security.authorization.annotation.Secured;
 import org.mokirim.infrastructure.security.context.MokirimPrincipal;
 import org.mokirim.infrastructure.security.context.MokirimSecurityContext;
 
+import io.jsonwebtoken.JwtException;
+import io.quarkus.security.UnauthorizedException;
 import jakarta.annotation.Priority;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ResourceInfo;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
@@ -20,20 +24,41 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 	@Inject
 	TokenDecoder tokenDecoder;
 
+	@Inject
+	ResourceInfo resourceInfo;
+
 	@Override
 	public void filter(ContainerRequestContext requestContext) {
+
+		Secured secured = getSecuredAnnotation();
+
+		if (secured == null) {
+			return;
+		}
+
 		String authHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 
-		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+		if (authHeader == null || !authHeader.startsWith("Mokirim ")) {
 			requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
 			return;
 		}
 
-		final var rawToken = authHeader.substring("Bearer ".length());
-		final var token = tokenDecoder.decode(rawToken);
-		final var mokirimPrincipal = new MokirimPrincipal(token.getUserId(), token.getUsername());
-		final var securityContext = new MokirimSecurityContext(mokirimPrincipal);
+		try {
+			final var rawToken = authHeader.substring("Mokirim ".length());
+			final var token = tokenDecoder.decode(rawToken);
+			final var mokirimPrincipal = new MokirimPrincipal(token.getUserId(), token.getUsername());
+			final var securityContext = new MokirimSecurityContext(mokirimPrincipal);
+			requestContext.setSecurityContext(securityContext);
+		} catch (JwtException error) {
+			throw new UnauthorizedException("Invalid token " + error);
+		}
 
-		requestContext.setSecurityContext(securityContext);
 	}
+
+	private Secured getSecuredAnnotation() {
+        if (resourceInfo.getResourceMethod().isAnnotationPresent(Secured.class)) {
+            return resourceInfo.getResourceMethod().getAnnotation(Secured.class);
+        }
+        return resourceInfo.getResourceClass().getAnnotation(Secured.class);
+    }
 }
