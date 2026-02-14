085f17d9e182ae44ff67f174d7b33c051dd54630
gfloriano11 <gflorianodev@gmail.com>
2026-02-14 00:50:12 -0300

    feat: add authorization filter to validate if user can acess route

--- src/main/java/org/mokirim/infrastructure/security/authentication/AuthenticationFilter.java
+++ src/main/java/org/mokirim/infrastructure/security/authentication/AuthenticationFilter.java
@@ -1,7 +1,6 @@
 package org.mokirim.infrastructure.security.authentication;
 
 import org.mokirim.infrastructure.security.authentication.token.TokenDecoder;
-import org.mokirim.infrastructure.security.authorization.annotation.Secured;
 import org.mokirim.infrastructure.security.context.MokirimPrincipal;
 import org.mokirim.infrastructure.security.context.MokirimSecurityContext;
 
@@ -30,12 +29,6 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 	@Override
 	public void filter(ContainerRequestContext requestContext) {
 
-		Secured secured = getSecuredAnnotation();
-
-		if (secured == null) {
-			return;
-		}
-
 		String authHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
 
 		if (authHeader == null || !authHeader.startsWith("Mokirim ")) {
@@ -46,19 +39,11 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 		try {
 			final var rawToken = authHeader.substring("Mokirim ".length());
 			final var token = tokenDecoder.decode(rawToken);
-			final var mokirimPrincipal = new MokirimPrincipal(token.getUserId(), token.getUsername());
+			final var mokirimPrincipal = new MokirimPrincipal(token.getUserId(), token.getUsername(), token.getRole());
 			final var securityContext = new MokirimSecurityContext(mokirimPrincipal);
 			requestContext.setSecurityContext(securityContext);
 		} catch (JwtException error) {
 			throw new UnauthorizedException("Invalid token " + error);
 		}
-
 	}
-
-	private Secured getSecuredAnnotation() {
-        if (resourceInfo.getResourceMethod().isAnnotationPresent(Secured.class)) {
-            return resourceInfo.getResourceMethod().getAnnotation(Secured.class);
-        }
-        return resourceInfo.getResourceClass().getAnnotation(Secured.class);
-    }
 }
---
+++ src/main/java/org/mokirim/infrastructure/security/authorization/AuthorizationFilter.java
@@ -0,0 +1,53 @@
+package org.mokirim.infrastructure.security.authorization;
+
+import org.mokirim.infrastructure.security.authorization.annotation.Secured;
+import org.mokirim.infrastructure.security.authorization.role.Role;
+
+import jakarta.annotation.Priority;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ResourceInfo;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+@Priority(Priorities.AUTHORIZATION)
+public class AuthorizationFilter implements ContainerRequestFilter {
+
+	@Inject
+	ResourceInfo resourceInfo;
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) {
+
+		final var secured = getSecuredAnnotation();
+
+		if (secured == null) {
+			return;
+		}
+
+		final var securityContext = requestContext.getSecurityContext();
+
+		if (securityContext != null) {
+			if (securityContext.getUserPrincipal() == null) {
+				requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+				return;
+			}
+
+			for (Role role : secured.value()) {
+				if (securityContext.isUserInRole(role.name())) {
+					return;
+				}
+			}
+		}
+	}
+
+	private Secured getSecuredAnnotation() {
+        if (resourceInfo.getResourceMethod().isAnnotationPresent(Secured.class)) {
+            return resourceInfo.getResourceMethod().getAnnotation(Secured.class);
+        }
+        return resourceInfo.getResourceClass().getAnnotation(Secured.class);
+    }
+}
